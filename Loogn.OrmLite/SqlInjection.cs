65d96f508baa9d0ea343abb0bb4d7d5fbb40fa1f
loogn <loogn_0707@126.com>
2015-05-21 10:01:20 +0800

    sqlinjection

---
+++ Loogn.OrmLite/SqlInjection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loogn.OrmLite
+{
+    /// <summary>
+    /// sql注入
+    /// </summary>
+    public class SqlInjection
+    {
+        /// <summary>
+        /// 过滤单引号注入
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string Filter(string value)
+        {
+            return value == null ? string.Empty : value.Replace("'", "''");
+        }
+    }
+}

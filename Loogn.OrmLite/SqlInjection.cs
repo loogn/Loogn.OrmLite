8fd58b992b340804f29a11a3e535b665679b3181
loogn <loogn_0707@126.com>
2016-04-12 18:04:04 +0800

    replaceforlike

--- Loogn.OrmLite/SqlInjection.cs
+++ Loogn.OrmLite/SqlInjection.cs
@@ -20,5 +20,25 @@ namespace Loogn.OrmLite
         {
             return value == null ? string.Empty : value.Replace("'", "''");
         }
+
+        /// <summary>
+        /// 对于like操作，需要进行替换
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ReplaceForLike(string value)
+        {
+            /*
+            对于like操作，需要进行以下替换（注意顺序也很重要）
+            [ -> [[]     (这个必须是第一个替换的!!)
+            % -> [%]    (这里%是指希望匹配的字符本身包括的%而不是专门用于匹配的通配符)
+            _ -> [_]
+            ^ -> [^]
+            */
+            return value == null ? string.Empty : value.Replace("[", "[[]")
+                .Replace("%", "[%]")
+                .Replace("_", "[_]")
+                .Replace("^", "[^]");
+        }
     }
 }

3d8e817776ab0f781716c32b2e60ff463d1cbc2b
loogn <loogn_0707@126.com>
2018-09-11 18:13:14 +0800

    使用同步字典缓存反射数据

--- Loogn.OrmLite/Utils/TypeCachedDict.cs
+++ Loogn.OrmLite/Utils/TypeCachedDict.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,38 +9,44 @@ namespace Loogn.OrmLite
 {
     static class TypeCachedDict
     {
-        static Dictionary<Type, TypeCachedInfo> Dict = new Dictionary<Type, TypeCachedInfo>();
+        //static Dictionary<Type, TypeCachedInfo> Dict = new Dictionary<Type, TypeCachedInfo>();
+        static ConcurrentDictionary<Type, TypeCachedInfo> Dict = new ConcurrentDictionary<Type, TypeCachedInfo>();
 
         public static TypeCachedInfo GetTypeCachedInfo(Type type)
         {
-            TypeCachedInfo info;
-            if (Dict.TryGetValue(type, out info))
-            {
-                return info;
-            }
-            else
-            {
-                info = new TypeCachedInfo(type);
-                Dict[type] = info;
-                return info;
-            }
+            return Dict.GetOrAdd(type, (key) => new TypeCachedInfo(type));
+
+            //TypeCachedInfo info;
+            //if (Dict.TryGetValue(type, out info))
+            //{
+            //    return info;
+            //}
+            //else
+            //{
+            //    info = new TypeCachedInfo(type);
+            //    Dict[type] = info;
+            //    return info;
+            //}
         }
-        
-        static Dictionary<Type, object> GDict = new Dictionary<Type, object>(50);
+
+        static ConcurrentDictionary<Type, object> GDict = new ConcurrentDictionary<Type, object>();
+        //static Dictionary<Type, object> GDict = new Dictionary<Type, object>(50);
         public static TypeCachedInfo<TObject> GetTypeCachedInfo<TObject>()
         {
             var type = typeof(TObject);
-            object info;
-            if (GDict.TryGetValue(type, out info))
-            {
-                return (TypeCachedInfo<TObject>)info;
-            }
-            else
-            {
-                var refInfo = new TypeCachedInfo<TObject>(type);
-                GDict[type] = refInfo;
-                return refInfo;
-            }
+            return (TypeCachedInfo<TObject>)GDict.GetOrAdd(type, (key) => new TypeCachedInfo<TObject>(key));
+
+            //object info;
+            //if (GDict.TryGetValue(type, out info))
+            //{
+            //    return (TypeCachedInfo<TObject>)info;
+            //}
+            //else
+            //{
+            //    var refInfo = new TypeCachedInfo<TObject>(type);
+            //    GDict[type] = refInfo;
+            //    return refInfo;
+            //}
         }
 
     }
